be45d77beb3e4e97ddb355836de900dd2a73ebf4
Olha Pylypova <olhasve@gmail.com>
2020-12-19 23:50:07 +0200

    hw8

--- 1.js
+++ 1.js
@@ -4,11 +4,14 @@ const items = [...document.querySelectorAll('li.item')];
 console.log(`В списке ${items.length} категории`);
  items.map(item => {
     const title = item.querySelector("h2");
-     const list = item.querySelectorAll("ul, li"); return { title: title.textContent, count: list.length };
+      const list = item.querySelectorAll("ul, li");
+     //  return { title: title.textContent, count: list.length };
+      console.log(title.textContent,list.length)
 }  
- ).forEach(entire => {
-     console.log(entire.title, entire.count);    
-});
+ )
+//       .forEach(entire => {
+//      console.log(entire.title, entire.count);    
+// });
 
 
 
